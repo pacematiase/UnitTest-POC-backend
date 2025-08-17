effcf92aa47847ed0ae803d8a4c430ece6737e82
Matias Emanuel Pace <matiasepace@hotmail.com>
2025-08-17 13:23:26 -0300

    Fix mocked repository

--- src/QueueEntry/__mocks__/queueEntry-repository.js
+++ src/QueueEntry/__mocks__/queueEntry-repository.js
@@ -2,40 +2,30 @@ export default class QueueEntryRepository {
   async getAll() {
     return [
       {
-        queueEntryDni: 12694519,
-        queueEntryDate: new Date('2025-08-06T21:59:33.000Z'),
+        queueEntryDni: 40225782,
+        queueEntryDate: new Date('2025-08-06T08:05:33.000Z'),
         queueEntryType: 2,
-        createdAt: new Date('2025-08-06T21:59:33.000Z'),
-        updatedAt: new Date('2025-08-06T21:59:33.000Z'),
       },
       {
-        queueEntryDni: 18887325,
-        queueEntryDate: new Date('2025-08-09T17:27:02.000Z'),
+        queueEntryDni: 16478996,
+        queueEntryDate: new Date('2025-08-06T08:08:05.000Z'),
         queueEntryType: 3,
-        createdAt: new Date('2025-08-09T17:27:02.000Z'),
-        updatedAt: new Date('2025-08-09T17:27:21.000Z'),
       },
       {
         queueEntryDni: 20154778,
-        queueEntryDate: new Date('2025-08-06T21:59:46.000Z'),
-        queueEntryType: 3,
-        createdAt: new Date('2025-08-06T21:59:46.000Z'),
-        updatedAt: new Date('2025-08-06T21:59:46.000Z'),
+        queueEntryDate: new Date('2025-08-06T07:50:46.000Z'),
+        queueEntryType: 1,
       },
       {
-        queueEntryDni: 28787525,
-        queueEntryDate: new Date('2025-08-09T17:32:39.000Z'),
-        queueEntryType: 3,
-        createdAt: new Date('2025-08-09T17:32:39.000Z'),
-        updatedAt: new Date('2025-08-09T17:32:39.000Z'),
+        queueEntryDni: 35578661,
+        queueEntryDate: new Date('2025-08-06T08:02:39.000Z'),
+        queueEntryType: 1,
       },
       {
         queueEntryDni: 34609500,
-        queueEntryDate: new Date('2025-08-06T21:59:27.000Z'),
-        queueEntryType: 1,
-        createdAt: new Date('2025-08-06T21:59:27.000Z'),
-        updatedAt: new Date('2025-08-06T21:59:27.000Z'),
-      },
-    ];
-  }
+        queueEntryDate: new Date('2025-08-06T07:20:27.000Z'),
+        queueEntryType: 3,
+      },
+    ];
+  }
 }
